7eb7796cc8991a969bc7d552be54eccdef58be75
Judith Kahrer <judith@curity.io>
2021-09-27 09:43:09 +0200

    Remove copyright license from pre-processing-procedure

--- pre-processing-procedures/open-banking-brasil-dcr-validation-template.js
+++ pre-processing-procedures/open-banking-brasil-dcr-validation-template.js
@@ -1,14 +1,3 @@
-/*
- * Copyright (C) 2021 Curity AB. All rights reserved.
- *
- * The contents of this file are the property of Curity AB.
- * You may not copy or use this file, in either source code
- * or executable form, except in compliance with terms
- * set by Curity AB.
- *
- * For further information, please contact Curity AB.
- */
-
 //Note: ES5!
 function result(context) {
     var maximumLifetimeInSeconds = 60*5;
